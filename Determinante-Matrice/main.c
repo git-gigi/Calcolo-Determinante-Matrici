f432f5793df3d41537f174cd06a6cea0573320c9
Gigi <luigi.meola@studio.unibo.it>
2023-11-16 14:26:18 +0100

    Initial Commit

---
+++ Determinante-Matrice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Determinante-Matrice
+//
+//  Created by Luigi Meola on 16/11/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
